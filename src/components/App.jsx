4b7ed24734c27b6bbdb54ee649eb8c35c1e9b101
Ramon San Gabriel <jeshero11@gmail.com>
2024-08-23 14:29:34 +0800

    homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 export const App = () => {
   return (
     <div
@@ -7,10 +8,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      goit-react-hw-05-movies
     </div>
   );
 };
