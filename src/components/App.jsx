ab14337b78b5143229d2af3950eb3398a97b81cf
Ramon San Gabriel <jeshero11@gmail.com>
2024-08-26 14:19:47 +0800

    MoviePage Search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SharedLayout from './SharedLayout/SharedLayout';
+import HomePage from 'pages/HomePage/HomePage';
+import MoviePage from 'pages/MoviePage/MoviePage';
+import MovieDetailsPage from 'pages/MovieDetailsPage/MovieDetailsPage';
+import CastList from './CastList/CastList';
+import ReviewList from './ReviewList/ReviewList';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      goit-react-hw-05-movies
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviePage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<CastList />} />
+            <Route path="reviews" element={<ReviewList />} />
+          </Route>
+          {/* serves as the default error page or component when the path is not accessible */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+export default App;
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Loader } from 'components/Loader/Loader';
+import Header from 'components/Header/Header';
+import css from './SharedLayout.module.css';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <div className={css.container}>
+        <Header />
+        <hr></hr>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </>
+  );
+};
+
+export default SharedLayout;
